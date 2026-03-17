ff99d396ba818a5b059b6629306565cd953536b1
marthinsalakory <marthinsalakory11@gmail.com>
2026-03-17 15:44:20 +0900

    Add label to CreateAction in ListIngatkanSayas page for improved clarity

--- app/Filament/Resources/IngatkanSayas/Pages/ListIngatkanSayas.php
+++ app/Filament/Resources/IngatkanSayas/Pages/ListIngatkanSayas.php
@@ -13,7 +13,8 @@ class ListIngatkanSayas extends ListRecords
     protected function getHeaderActions(): array
     {
         return [
-            CreateAction::make(),
+            CreateAction::make()
+                ->label('Tambah Daftar Pengingat'),
         ];
     }
 }
